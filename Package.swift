37215ca1f126341b4167154ba94afb86035f26da
Karl Bowden <karl@bearded.sexy>
2016-09-19 13:18:48 +1000

    SPM: Targets needs to appear before dependencies

--- Package.swift
+++ Package.swift
@@ -2,12 +2,18 @@ import PackageDescription
 
 let package = Package(
     name: "BrightFutures",
-    dependencies: [
-        .Package(url: "https://github.com/antitypical/Result", majorVersion: 3)
-    ],
     targets: [
         Target(
             name: "BrightFutures"
         )
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/antitypical/Result", majorVersion: 3)
+    ],
+    exclude: [
+        "BrightFutures.xcodeproj",
+        "BrightFutures.xcworkspace",
+        "BrightFuturesTests",
+        "Documentation",
     ]
 )
